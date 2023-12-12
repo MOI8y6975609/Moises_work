3e3aa496e45a626d2fd6b93fa3cbc6523f4ecabe
MOI8y6975609 <144734129+MOI8y6975609@users.noreply.github.com>
2023-12-12 14:17:35 -0700

    Update indexscript.js

--- indexscript.js
+++ indexscript.js
@@ -4,3 +4,14 @@ const person = {
     age: 36,
   favoritecolor: "blue"
 };
+let nameOfElement;
+
+let date1 = new Date("9/12/23");
+let date2 = new Date();
+
+// To calculate the time difference of two dates
+let Difference_In_Time = date2.getTime() - date1.getTime();
+
+// To calculate the no. of days between two dates
+let Difference_In_Days = 
+	Math.round(Difference_In_Time / (1000 * 3600 * 24));
